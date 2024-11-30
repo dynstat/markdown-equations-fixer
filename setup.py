cc0c2b454a839462b8b75163e6f7661c5790cd7c
VIVEK SINGH <vs15vivek@gmail.com>
2024-12-01 01:20:34 +0530

    version changed to 1.1.0

--- setup.py
+++ setup.py
@@ -3,12 +3,13 @@ from setuptools import setup, find_packages
 
 setup(
     name="markdown-equations-fixer",
-    version="1.0.0",
+    version="1.1.0",
     packages=find_packages(where="src"),
     package_dir={"": "src"},
     install_requires=[
         "click>=8.0.0",
         "rich>=10.0.0",
+        "pypandoc",
     ],
     entry_points={
         "console_scripts": [
@@ -17,7 +18,7 @@ setup(
     },
     author="Vivek Singh",
     author_email="vs15vivek@gmail.com",
-    description="A CLI tool to fix mathematical equations in markdown files",
+    description="A CLI tool to fix mathematical equations in markdown files and convert between formats",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
     url="https://github.com/vs15vivek/markdown-equations-fixer",
